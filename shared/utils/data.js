4e9c67153b6db2f9426de6d1bd145b6f65200821
malerba118 <malerba118@gmail.com>
2021-10-15 12:53:29 -0600

    stuff

--- shared/utils/data.js
+++ shared/utils/data.js
@@ -2,146 +2,229 @@ import { types } from "mobx-state-tree";
 import { nanoid } from "nanoid";
 import { isEmpty, createDefaultNode } from "shared/components/RichtextViewer";
 
-export const getPreviewablePortfolio = ({ name }) => {
-  return processPortfolio(
-    {
-      id: "123",
-      advertisementsDisabled: true,
-      subdomain: null,
-      published: null,
-      draft: {
-        template: name,
-        content: {
-          about: {
-            title: "Software Engineer",
-            summary: "I build apps and libraries ",
+const EXAMPLE_PORTFOLIOS = {
+  wedding: {
+    id: "DroDGjtdHfVq2qeQPwAA",
+    published: null,
+    draftLastSaved: "2021-10-15T17:32:27.726Z",
+    subdomain: "ellie-wedding",
+    draft: {
+      content: {
+        contact: {
+          socialLinks: {
+            items: [],
+          },
+          email: {
+            hidden: false,
+            value: "",
+          },
+          phone: {
+            value: "",
+            hidden: false,
+          },
+        },
+        projects: [
+          {
+            endDate: null,
+            name: "Karime + Austin",
             description:
-              '[{"type":"paragraph","children":[{"text":"Hi, I\'m Austin and I\'m a web developer. I\'ve been building web apps and libraries for 8 years. I attended the Rochester Institute of Technology in New York from 2012-2017 and graduated with a BS in Comuter Science and a 3.95 GPA. After college I began my web development career at Target where I worked on a variety of teams over the course of 3 years. In 2020 I decided to move on from Target and began working for a startup that is developing a web-based video editor."}]},{"type":"paragraph","children":[{"text":""}]}]',
-            firstName: "Austin",
-            lastName: "Malerba",
+              '[{"type":"paragraph","children":[{"text":"When I met Karime & Austin, I could instantly feel the chemistry between them. They had a destination wedding in Brooklyn, coming all the way from Texas. We chatted and laughed   the whole way through their pre-wedding shoot as we walked through Brooklyn and admired the Manhattan skyline. Here they are, fully in love."}]}]',
+            startDate: 1622520000000,
+            summary: "Brooklyn, NY",
             images: {
               items: [
                 {
+                  height: 1147,
+                  name: null,
+                  crop: null,
+                  type: "image/jpeg",
                   rawUrl:
-                    "https://firebasestorage.googleapis.com/v0/b/portfolios-8be96.appspot.com/o/users%2F23ZIQKNd42bJydGnIlYFb6Btnq43%2Fpublic%2Fraw-AuJ85wr9S6Eo8ccOEoXWp?alt=media&token=616c300d-9070-4149-aa01-118e05b3de37",
-                  width: 1155,
-                  processedUrl:
-                    "https://firebasestorage.googleapis.com/v0/b/portfolios-8be96.appspot.com/o/users%2F23ZIQKNd42bJydGnIlYFb6Btnq43%2Fpublic%2Fprocessed-AuJ85wr9S6Eo8ccOEoXWp?alt=media&token=1c9c27c9-2755-4a9f-943d-a12fc1f9483b",
-                  id: "AuJ85wr9S6Eo8ccOEoXWp",
-                  height: 1732,
-                  zoom: 1,
+                    "https://firebasestorage.googleapis.com/v0/b/portfolios-8be96.appspot.com/o/users%2FfBNxurrfgUbbFOLTsDsp1xdPqjF3%2Fpublic%2Fraw-aexEhIyewd6irLHcUjNF7?alt=media&token=6c6c8f32-510a-4bf9-b0b1-9d122c94d521",
+                  processedUrl: null,
+                  zoom: null,
+                  width: 1732,
+                  id: "aexEhIyewd6irLHcUjNF7",
+                },
+                {
+                  width: 1147,
                   crop: {
-                    height: 43.95,
-                    y: 0,
-                    width: 84.58028340840839,
-                    x: 15.41971659159157,
+                    x: 0.4814954682779456,
+                    width: 98.07637839879155,
+                    height: 98.7546875,
+                    y: 0.5078125,
                     unit: "%",
                   },
+                  id: "oYLc5CZpKJcMk3ndMHIZq",
+                  type: "image/jpeg",
+                  zoom: 1,
+                  rawUrl:
+                    "https://firebasestorage.googleapis.com/v0/b/portfolios-8be96.appspot.com/o/users%2FfBNxurrfgUbbFOLTsDsp1xdPqjF3%2Fpublic%2Fraw-oYLc5CZpKJcMk3ndMHIZq?alt=media&token=4304a4f2-7ad5-41d6-9c01-8d270d2c15c8",
+                  name: null,
+                  height: 1732,
+                  processedUrl:
+                    "https://firebasestorage.googleapis.com/v0/b/portfolios-8be96.appspot.com/o/users%2FfBNxurrfgUbbFOLTsDsp1xdPqjF3%2Fpublic%2Fprocessed-oYLc5CZpKJcMk3ndMHIZq?alt=media&token=e258ec81-8f8d-4035-965c-9ceb091de928",
+                },
+                {
+                  name: null,
+                  height: 1732,
+                  width: 1329,
+                  type: "image/jpeg",
+                  crop: {
+                    width: 91.3421630859375,
+                    y: 2.6781250000000245,
+                    unit: "%",
+                    height: 95.08984375,
+                    x: 2.6631673177083357,
+                  },
+                  id: "MUavX2gSur451v1ra-onz",
+                  zoom: 1,
+                  processedUrl:
+                    "https://firebasestorage.googleapis.com/v0/b/portfolios-8be96.appspot.com/o/users%2FfBNxurrfgUbbFOLTsDsp1xdPqjF3%2Fpublic%2Fprocessed-MUavX2gSur451v1ra-onz?alt=media&token=03bd061f-01da-4657-90f7-1ee0f597248b",
+                  rawUrl:
+                    "https://firebasestorage.googleapis.com/v0/b/portfolios-8be96.appspot.com/o/users%2FfBNxurrfgUbbFOLTsDsp1xdPqjF3%2Fpublic%2Fraw-MUavX2gSur451v1ra-onz?alt=media&token=c28dab55-331c-4a19-96bd-f6a335e74eee",
+                },
+                {
+                  height: 1732,
+                  id: "pzCXlNryhOzFrIAuPofVf",
+                  type: "image/jpeg",
+                  crop: {
+                    width: 91.326904296875,
+                    x: 4.157511393229166,
+                    y: 3.1859375000000254,
+                    height: 95.21953125,
+                    unit: "%",
+                  },
+                  width: 1329,
+                  zoom: 1,
+                  rawUrl:
+                    "https://firebasestorage.googleapis.com/v0/b/portfolios-8be96.appspot.com/o/users%2FfBNxurrfgUbbFOLTsDsp1xdPqjF3%2Fpublic%2Fraw-pzCXlNryhOzFrIAuPofVf?alt=media&token=2802006c-23fa-456a-80d6-38ce273701d3",
+                  processedUrl:
+                    "https://firebasestorage.googleapis.com/v0/b/portfolios-8be96.appspot.com/o/users%2FfBNxurrfgUbbFOLTsDsp1xdPqjF3%2Fpublic%2Fprocessed-pzCXlNryhOzFrIAuPofVf?alt=media&token=124da867-548d-4a0e-8c4b-4fcc9aa849f3",
+                  name: null,
+                },
+                {
+                  rawUrl:
+                    "https://firebasestorage.googleapis.com/v0/b/portfolios-8be96.appspot.com/o/users%2FfBNxurrfgUbbFOLTsDsp1xdPqjF3%2Fpublic%2Fraw-3KuGfZjztNlCTtcdOp3H8?alt=media&token=489f150d-a8c4-4ac6-9c0b-826b568027bf",
+                  processedUrl:
+                    "https://firebasestorage.googleapis.com/v0/b/portfolios-8be96.appspot.com/o/users%2FfBNxurrfgUbbFOLTsDsp1xdPqjF3%2Fpublic%2Fprocessed-3KuGfZjztNlCTtcdOp3H8?alt=media&token=baca4952-9235-4715-bf20-52764411b5b1",
+                  zoom: 1,
+                  height: 1732,
+                  id: "3KuGfZjztNlCTtcdOp3H8",
+                  type: "image/jpeg",
+                  width: 1329,
+                  crop: {
+                    height: 96.278125,
+                    y: 1.8109375000000452,
+                    x: 2.5166829427083313,
+                    width: 93.109130859375,
+                    unit: "%",
+                  },
+                  name: null,
+                },
+                {
+                  processedUrl:
+                    "https://firebasestorage.googleapis.com/v0/b/portfolios-8be96.appspot.com/o/users%2FfBNxurrfgUbbFOLTsDsp1xdPqjF3%2Fpublic%2Fprocessed-PttgIVPlx_cQqCv7vfDUN?alt=media&token=2ccf25fb-959d-4e5b-8c71-5ae56019c042",
+                  rawUrl:
+                    "https://firebasestorage.googleapis.com/v0/b/portfolios-8be96.appspot.com/o/users%2FfBNxurrfgUbbFOLTsDsp1xdPqjF3%2Fpublic%2Fraw-PttgIVPlx_cQqCv7vfDUN?alt=media&token=0048eb3a-ea79-4e76-a154-cf2ab48e2c60",
+                  id: "PttgIVPlx_cQqCv7vfDUN",
+                  height: 1329,
+                  zoom: 1,
+                  type: "image/jpeg",
+                  crop: {
+                    unit: "%",
+                    y: 3.31484375,
+                    x: 1.2581480061349692,
+                    height: 93.01640625,
+                    width: 96.83126437883438,
+                  },
+                  width: 1732,
+                  name: null,
                 },
               ],
             },
-            resume: null,
-            logo: {
-              items: [],
-            },
+            id: "fTV--61eH1WqbtZeP3yP-",
           },
-          projects: [
-            {
-              startDate: null,
-              id: "SAQrywURBzFQ4I_cyo78q",
-              endDate: null,
-              name: "",
-              summary: "",
-              description: "",
-              images: {
-                items: [],
+        ],
+        about: {
+          resume: null,
+          images: {
+            items: [
+              {
+                processedUrl:
+                  "https://firebasestorage.googleapis.com/v0/b/portfolios-8be96.appspot.com/o/users%2FfBNxurrfgUbbFOLTsDsp1xdPqjF3%2Fpublic%2Fprocessed-8YaITZIj9uCja-x2y6eZ9?alt=media&token=0172cc51-1652-4899-b759-a3736014b1fb",
+                zoom: 1,
+                type: "image/jpeg",
+                crop: {
+                  y: 9.4157958984375,
+                  unit: "%",
+                  height: 65.40458374023437,
+                  x: 0,
+                  width: 100,
+                },
+                height: 2990,
+                name: "pexels-andrea-piacquadio-774909.jpg",
+                id: "8YaITZIj9uCja-x2y6eZ9",
+                rawUrl:
+                  "https://firebasestorage.googleapis.com/v0/b/portfolios-8be96.appspot.com/o/users%2FfBNxurrfgUbbFOLTsDsp1xdPqjF3%2Fpublic%2Fraw-8YaITZIj9uCja-x2y6eZ9?alt=media&token=5f5a3352-5297-455b-977f-23a0a4874927",
+                width: 1992,
               },
-            },
-            {
-              endDate: 1619848800000,
-              id: "0.14531344198085439",
-              name: "Iconik Studio",
-              startDate: 1609484400000,
-              summary: "Svg Editor",
-              images: {
-                items: [
-                  {
-                    id: "DIIF4O0vKLxzM-hGNYqte",
-                    rawUrl:
-                      "https://firebasestorage.googleapis.com/v0/b/portfolios-8be96.appspot.com/o/users%2F23ZIQKNd42bJydGnIlYFb6Btnq43%2Fpublic%2Fraw-DIIF4O0vKLxzM-hGNYqte?alt=media&token=d130823c-142a-4c7e-8664-2924e9789f5f",
-                    crop: null,
-                    width: 920,
-                    zoom: null,
-                    height: 640,
-                    processedUrl: null,
-                  },
-                  {
-                    height: 1920,
-                    processedUrl:
-                      "https://firebasestorage.googleapis.com/v0/b/portfolios-8be96.appspot.com/o/users%2F23ZIQKNd42bJydGnIlYFb6Btnq43%2Fpublic%2Fprocessed-vgVKU9HmKiGK1m2XzxoLn?alt=media&token=be9b1935-2554-4872-82bd-7c7e49cc7eb8",
-                    rawUrl:
-                      "https://firebasestorage.googleapis.com/v0/b/portfolios-8be96.appspot.com/o/users%2F23ZIQKNd42bJydGnIlYFb6Btnq43%2Fpublic%2Fraw-vgVKU9HmKiGK1m2XzxoLn?alt=media&token=f7062001-cdd0-42f6-8802-f34c2b863715",
-                    width: 1439,
-                    zoom: 1,
-                    id: "vgVKU9HmKiGK1m2XzxoLn",
-                    crop: {
-                      width: 100,
-                      unit: "%",
-                      height: 49.35390624999999,
-                      y: 22.715624999999978,
-                      x: 0,
-                    },
-                  },
-                ],
-              },
-              description: '[{"type":"paragraph","children":[{"text":""}]}]',
-            },
-            {
-              endDate: null,
-              name: "Engagement",
-              summary: "Got engaged",
-              startDate: 1590991200000,
-              description:
-                '[{"type":"paragraph","children":[{"text":"Got engaged"}]}]',
-              images: {
-                items: [
-                  {
-                    zoom: 1,
-                    id: "xM5RunUGXLs3B_QeFERTv",
-                    height: 1732,
-                    crop: {
-                      y: 0,
-                      width: 100,
-                      unit: "%",
-                      height: 63.7625,
-                      x: 0,
-                    },
-                    processedUrl:
-                      "https://firebasestorage.googleapis.com/v0/b/portfolios-8be96.appspot.com/o/users%2F23ZIQKNd42bJydGnIlYFb6Btnq43%2Fpublic%2Fprocessed-xM5RunUGXLs3B_QeFERTv?alt=media&token=0e40a902-b166-4995-a297-6ed7a03c2063",
-                    rawUrl:
-                      "https://firebasestorage.googleapis.com/v0/b/portfolios-8be96.appspot.com/o/users%2F23ZIQKNd42bJydGnIlYFb6Btnq43%2Fpublic%2Fraw-xM5RunUGXLs3B_QeFERTv?alt=media&token=c971a728-6250-4438-9386-3bdc7e9b93b4",
-                    width: 1155,
-                  },
-                ],
-              },
-              id: "0.5596382356762701",
-            },
-          ],
-          contact: {
-            email: {
-              hidden: false,
-              value: "austin.malerba@gmail.com",
-            },
-            phone: {
-              hidden: false,
-              value: "915-867-5309",
-            },
+            ],
           },
+          title: "Wedding Photographer",
+          logo: {
+            items: [],
+          },
+          lastName: "Gutierrez",
+          description:
+            '[{"type":"paragraph","children":[{"text":"Hi, I\'m Ellie! I\'m a wedding photographer based in Brooklyn, NY. At heart, I\'m a storyteller and I want to capture your story. I promise these photos will only grow more valuable over the years and your story will be timeless."}]}]',
+          summary: "I write love stories using pixels.",
+          firstName: "Ellie",
         },
       },
+      templateSettingsMap: {
+        skrol: {
+          paragraphFont: "Lato",
+          palette: "purplePink",
+          headingFont: "Montserrat",
+        },
+        gallery: {
+          palette: "gray",
+          paragraphFont: "EB Garamond",
+          headingFont: "Crimson Text",
+        },
+        venice: {
+          palette: "ocean",
+          paragraphFont: "Ubuntu",
+          headingFont: "Ubuntu",
+        },
+        madrid: {
+          headingFont: "Montserrat",
+          palette: "desert",
+          paragraphFont: "Lato",
+        },
+        os: {
+          palette: "gray",
+          paragraphFont: "Ubuntu Mono",
+          headingFont: "Ubuntu Mono",
+        },
+      },
+      template: "gallery",
     },
-    { useDraft: true }
-  );
+    advertisementsDisabled: true,
+    owner: "fBNxurrfgUbbFOLTsDsp1xdPqjF3",
+  },
+};
+
+export const getPreviewablePortfolio = ({ name }) => {
+  const folio = EXAMPLE_PORTFOLIOS["wedding"];
+  const folioCopy = {
+    ...folio,
+    draft: {
+      ...folio.draft,
+      template: name,
+    },
+  };
+  return processPortfolio(folioCopy, { useDraft: true });
 };
 
 export const templates = {
